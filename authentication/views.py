b0a30a4572d938c459157689c62df9f5e1410c5c
Bhautik Savaliya <bhautiksavaliya2000@gmail.com>
2024-07-13 17:12:26 -0400

    Added firebase configuration.

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from EcoTourGuide.firebase_config import db, auth, storage
 
 
-def index(request):
-    return render(request, '', {})
+def login(request):
+
+    # test code.
+    users_ref = db.collection('User')
+    docs = users_ref.stream()
+
+    users = [doc.to_dict() for doc in docs]
+
+    return HttpResponse(f"Users: {users}")
+    # return render(request, '', {})

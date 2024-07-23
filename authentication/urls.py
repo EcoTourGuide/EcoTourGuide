f4fcf775337a00a6d9ea3d05651006b9ee2dd41f
krishghelani7790 <krishghelani7777@gmail.com>
2024-07-23 16:59:42 -0400

    change the login and signup templates to make better Ui and add the logout functionality make the forms.py to add more security in signup page and make details view in home section

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,6 +9,7 @@ app_name = 'authentication'
 urlpatterns = [
    path('login/', views.login_user, name='login'),
    path('signup/', views.signup_user, name='signup'),
+   path('logout/', views.logout_user, name='logout'),
    path('', views.index, name='explorer'),  # URL pattern for the explorer page
 
 ]
